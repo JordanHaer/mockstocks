285bf1afd09ab5fd2ad6befc52c1d966a384999f
JordanHaer <jordanhaer@gmail.com>
2023-06-23 15:22:55 +0100

    Added GH LN links

--- client/src/pages/Home/Footer.jsx
+++ client/src/pages/Home/Footer.jsx
@@ -4,9 +4,16 @@ export default function Footer() {
       <div className="container mx-auto flex max-w-5xl flex-col items-center gap-2 py-4 sm:flex-row sm:justify-between sm:gap-0 sm:px-8">
         <p className="text-center">&copy; 2023 Jordan Haer</p>
         <ul className="flex justify-center">
-          <li className="px-4 underline">Terms of Use</li>
-          <li className="border-l border-r border-white px-4 underline">Privacy Policy</li>
-          <li className="px-4 underline">Data Policy</li>
+          <li className="px-4 hover:underline">
+            <a href="https://github.com/JordanHaer" target="_blank" rel="noreferrer">
+              GitHub
+            </a>
+          </li>
+          <li className="border-l border-white px-4 hover:underline">
+            <a href="https://linkedin.com/in/JordanHaer" target="_blank" rel="noreferrer">
+              LinkedIn
+            </a>
+          </li>
         </ul>
       </div>
     </div>
